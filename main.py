6f348f58f5f4ee25255d8f63478597894904e07d
erwinpaillacan <43705290+erwinpaillacan@users.noreply.github.com>
2023-05-05 11:12:45 -0400

    [feat]: apt

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ from src.ml.data import process_data
 from src.ml.model import inference
 import pathlib
 
+import os
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
 app = FastAPI()
 
 
